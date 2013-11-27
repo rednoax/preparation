da547fbc1580fcc1cd5cf8f9f17b2363259de345
root <rednoax@gmail.com>
2013-11-27 17:40:26 +0000

    Add immediate load in input example
    either via mov or ldr reg, lable

--- inline_assembly_input.c
+++ inline_assembly_input.c
@@ -21,6 +21,19 @@ although it is useless.
 	: \
 	);\
 })
+//	r(0x8086) will add ldr	r3, .L2
+void mov_imme(void *ptr)
+{
+	int ret;
+	asm volatile(
+	"swp %0, %1, [%2]\n\t"
+	:"=r"(ret)
+	:"r"(0x8086), "r"(ptr));
+	asm volatile(
+	"swp %0, %1, [%2]\n\t"
+	:"=r"(ret)
+	:"r"(0x8), "r"(ptr));//will cause mov r3, #8
+}
 int main()
 {
 	raw_local_irq_enable();
@@ -40,8 +53,43 @@ int main()
 	.eabi_attribute 30, 6
 	.eabi_attribute 34, 0
 	.eabi_attribute 18, 4
-	.file	"inline_assembly.c"
+	.file	"inline_assembly_input.c"
 	.text
+	.align	2
+	.global	mov_imme
+	.type	mov_imme, %function
+mov_imme:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	stmfd	sp!, {r4, fp}
+	add	fp, sp, #4
+	sub	sp, sp, #16
+	str	r0, [fp, #-16]
+	ldr	r3, .L2
+	ldr	r2, [fp, #-16]
+#APP
+@ 28 "inline_assembly_input.c" 1
+	swp r4, r3, [r2]
+	
+@ 0 "" 2
+	str	r4, [fp, #-8]
+	mov	r3, #8
+	ldr	r2, [fp, #-16]
+#APP
+@ 32 "inline_assembly_input.c" 1
+	swp r4, r3, [r2]
+	
+@ 0 "" 2
+	str	r4, [fp, #-8]
+	sub	sp, fp, #4
+	ldmfd	sp!, {r4, fp}
+	bx	lr
+.L3:
+	.align	2
+.L2:
+	.word	32902
+	.size	mov_imme, .-mov_imme
 	.align	2
 	.global	main
 	.type	main, %function
@@ -54,7 +102,7 @@ main:
 	sub	sp, sp, #12
 	ldr	r3, [fp, #-8]
 #APP
-@ 16 "inline_assembly.c" 1
+@ 39 "inline_assembly_input.c" 1
 	mrs r3, cpsr
 	bic r3, r3, #1<<7 @IFTModes
 	msr cpsr_c, r3
@@ -68,4 +116,4 @@ main:
 	.size	main, .-main
 	.ident	"GCC: (Buildroot 2013.05) 4.7.3"
 	.section	.note.GNU-stack,"",%progbits
-#endif	
+#endif
