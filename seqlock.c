faaa49bbe06e5a3006d21f46724ade16e5f539b1
Rednoah <rednoax@gmail.com>
2015-11-28 15:10:46 +0800

    Add typecast example when dynamic assignment

--- seqlock.c
+++ seqlock.c
@@ -19,7 +19,7 @@ typedef struct{
 
 seqlock_t xtime_lock = {//the member not stated here will be 0!
 	10,//during assignment sequence, use ',' rather than ';'
-	(spinlock_t){//absolutely necessary
+	(spinlock_t){//not necessary, the typecast can be removed because here the assignment is static
 		.raw_lock = {},//empty assignment for empty structure
 		.dep_map = {.name = "xtime_lock"},
 	},//this comma can be ignored!
@@ -48,9 +48,40 @@ void dump_seqlock(seqlock_t *pseq)
 	struct lockdep_map *pmap = &pspin->dep_map;
 	printf("%d %p %p %s\n", pseq->sequence, pmap->key, pmap->class_cache, pmap->name);
 }
-int main()
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+#define LIST_HEAD_INIT(name) {&name, &name}
+/*
+no problem for the following 3
+*/
+struct list_head list1 = LIST_HEAD_INIT(list1);//{&list1, &list1};
+struct list_head list2 = (struct list_head)LIST_HEAD_INIT(list2);//typecast is not necessary since it is static assignment here!
+struct list_head list3 = {&list3};//untouched fields are 0 according the sequnce of defination
+struct list_head list4 = {.prev = &list4};//untouched fields are 0
+
+int main(int argc, char **argv)
 {
+	struct list_head *p;
 	dump_seqlock(&xtime_lock);
 	dump_seqlock(&xtime_lock2);
+	p = &list3;
+	printf("%p %p\n", p->prev, p->next);
+	p = &list4;
+	printf("%p %p\n", p->prev, p->next);
+	/*
+$ gcc -Wall seqlock.c
+seqlock.c: In function 'main':
+seqlock.c:55:30: error: expected expression before '{' token
+ #define LIST_HEAD_INIT(name) {&name, &name}
+                              ^
+seqlock.c:83:7: note: in expansion of macro 'LIST_HEAD_INIT'
+  *p = LIST_HEAD_INIT(list4);
+       ^
+	*/
+	//typecast is necessary, error otherwise like above
+	*p = (struct list_head)LIST_HEAD_INIT(list4);
+	printf("%p %p\n", p->prev, p->next);
 	return 0;
 }
