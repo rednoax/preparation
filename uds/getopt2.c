cd734229b88eb6cbfdec000fa335e17e91063ff9
sliu <sz_liusha@spectratech.com>
2017-12-23 18:31:41 +0800

    Add getopt example from its manual

---
+++ uds/getopt2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int opt, nsecs;
+	int nflags, tflags;
+	nflags = tflags = nsecs = 0;
+	while ((opt = getopt(argc, argv, "nt:")) != -1) {
+		switch(opt) {
+		case 'n':
+			nflags = 1;
+			break;
+		case 't':
+			nsecs = atoi(optarg);
+			tflags = 1;
+			break;
+		default:
+			/*
+			'?':if there is no arg for -t, '?' is returned and optopt is set to 't'
+			    or the option is not in the optstring
+			*/
+			fprintf(stderr, "Usage:%s [-t nsecs] [-n] name\n", argv[0]);
+			exit(-1);
+		}
+	}
+	printf("optind %d, nflags %d, tflags %d, nsecs %d\n", optind, nflags, tflags, nsecs);
+	//the following is a very importand routine to test nonoption argument
+	if (optind >= argc) {
+		fprintf(stderr, "expect argument after options\n");
+		exit(-1);
+	}
+	printf("name argument=%s\n", argv[optind]);
+	return 0;
+}
