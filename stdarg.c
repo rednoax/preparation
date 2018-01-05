d54eed9632b809f0d7f776470e2d4b5a56625bac
sliu <sz_liusha@spectratech.com>
2018-01-05 17:07:47 +0800

    equivalent of ...=>__VAR_ARGS and arg...=>arg

--- stdarg.c
+++ stdarg.c
@@ -1,5 +1,33 @@
 #include <stdarg.h>
 #include <stdio.h>
+//the following 2 is equivalent, __VA_ARGS__ tokens should be >=1, for __android_log_print demands fmt, so the __VA_ARGS__ here should >= 2
+#define android_printlog(prio, tag, fmt...) \
+	__android_log_print(prio, tag, fmt)
+
+#define android_printlog2(prio, tag, ...) \
+	__android_log_print(prio, tag, __VA_ARGS__)
+
+#define android_printlog3(prio, tag, fmt, arg...) \
+	__android_log_print(prio, tag, fmt, ##arg)
+/*
+stdarg.c:13:41: error: expected expression before ‘)’ token
+  __android_log_print(prio, tag, fmt, arg)
+                                         ^
+stdarg.c:69:2: note: in expansion of macro ‘android_printlog4’
+  android_printlog4(4, "Atfwd_Daemon", "***4\n");
+*/
+#define android_printlog4(prio, tag, fmt, arg...) \
+	__android_log_print(prio, tag, fmt, arg)
+int __android_log_print(int prio, const char *tag, const char *fmt, ...)
+{
+	va_list ap;
+	char buf[1024];
+	//char *bufp = buf;
+	va_start(ap, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, ap);
+	va_end(ap);
+	return printf(buf);
+}
 
 int msgprintf(char *fmt, ...)
 {
@@ -42,5 +70,9 @@ Illegal instruction (core dumped)
 int main(int argc, char **argv)
 {
 	msgprintf("%s:%c %d\n", __func__, 'X', __LINE__);
+	android_printlog(4, "Atfwd_Daemon", "***%s:%d\n", __func__, __LINE__);
+	android_printlog2(4, "Atfwd_Daemon", "***%s:%d\n", __func__, __LINE__);
+	android_printlog3(4, "Atfwd_Daemon", "***3\n");
+	//android_printlog4(4, "Atfwd_Daemon", "***4\n");//triggered error is list above
 	return 0;
 }
