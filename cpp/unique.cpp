1b20632153c20b8bf82578349ce90f94245f4123
sliu <sz_liusha@spectratech.com>
2018-02-15 16:59:40 +0800

    there is no copycons when using temp obj during obj instantiation
    while push_back(temp obj) cons twice, 1st temp obj 2nd copy cons
    
    TODO:unique_ptr's copy cons must be public even it is not used in title case. Why???
    others:
    
    1. fix operator=
    
    2. fix some debug info

--- cpp/unique.cpp
+++ cpp/unique.cpp
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string>
-
+#include <string.h>
 #if 0
 #define USE_STD_UNIQUE
 #else
-# if 10
+# if 0
 #define USE_UNIQUE_UNSTANDARD
 # endif
 #endif
@@ -30,7 +30,7 @@ private:
 		printf("%s:%p <==%p\n", __func__, this, src);
 		return *this;
 	}
-# if defined(USE_UNIQUE_UNSTANDARD)
+# if !defined(USE_UNIQUE_UNSTANDARD)
 public://build error if copy cons is private
 # endif
 	unique_ptr(const unique_ptr& src)
@@ -41,7 +41,7 @@ public://build error if copy cons is private
 public:
 	unique_ptr& operator=(int i)
 	{
-		m = NULL;
+		//m = NULL;
 		printf("%s:%p <==%d\n", __func__, this, i);
 		return *this;
 	}	
@@ -60,8 +60,9 @@ public:
 	}
 	~unique_ptr()
 	{
-		printf("%s: %p(%p), start delete %p: ", __func__, this, m, m);
-		delete m;
+		if (m)
+			delete m;
+		printf("%s: %p(%p), fin delete %p\n", __func__, this, m, m);
 	}
 //friend unique_ptr<T> make_unique(Args... args);
 };
@@ -89,6 +90,16 @@ private:
 	std::string m_Name;
 	int m_value;
 public:
+	MyClass(const MyClass& src)
+	{
+		m_Name = src.m_Name;
+		m_value = src.m_value;
+		printf("%s(const MyClass& %p): %p<=%p\n", __func__, &src, this, &src);
+	}
+	MyClass()
+	{
+		printf("%s(): %p\n", __func__, this);
+	}
 	MyClass(const std::string& name, int value)
 		:m_Name(name)
 		,m_value(value)
@@ -97,7 +108,7 @@ public:
 	}
 	~MyClass()
 	{
-		printf("%s: %p\n", __func__, this);
+		printf("%s:\"%s\" %p\n", __func__, m_Name.c_str(), this);
 	}
 	const std::string& GetName() const
 	{
@@ -107,17 +118,50 @@ public:
 	{
 		return m_value;
 	}
+	MyClass& operator=(const MyClass &src)
+	{
+		m_Name = src.m_Name;
+		m_value = src.m_value;
+		printf("%s: %p<=%p\n", __func__, this, &src);
+		return *this;
+	}
+	/*
+	bool operator()(const MyClass& right)
+	{
+		const char* p1 = this->GetName().c_str();
+		const char* p2 = right.GetName().c_str();
+		printf("%s < %s\n", p1, p2);
+		return strcmp(p1, p2);
+	}
+	*/
 };
+
 
 int main(int argc, char **argv)
 {
+	//template unique_ptr<MyClass>;
+	printf("###stack temp obj start:\n");
+	MyClass("test0", 0);
+	printf("###stack temp obj des fin\n");
+	printf("###these 2 equivalents are NOT temp obj copy cons!!!!!!!!\n");
+	printf("push_back(temp obj) will call cons twice: first temp obj cons then copy cons, which "\
+			"is not like the following \"obj instantiate using copy cons\"\n");
+	MyClass o0(MyClass("test1", 1));//temp obj call related cons, then no copy cons
+	MyClass o1 = MyClass("test2", 2);//exactly equal to the above
+	printf("###these 2 equivalents really call copy cons\n");
+	MyClass o2 = o0;
+	MyClass o3(o0);//absolutely equal to the above
+	printf("###stack obj from \"temp obj\" fin\n");
+	//printf("%d\n", (o0, o1));
+	//
+	printf("###these 3 quivalent instantiation");
 	unique_ptr<MyClass> uniquePointer(make_unique<MyClass>("MyClass", 10));
 	printf("__cplusplus: %ld; unique_ptr instance %p\n", __cplusplus, &uniquePointer);//201103
 	printf("%s: %d\n", uniquePointer->GetName().c_str(), uniquePointer->GetValue());
-	unique_ptr<MyClass> uniquePointer4 = make_unique<MyClass>("MyClass2", 20);
+	unique_ptr<MyClass> uniquePointer4 = make_unique<MyClass>("MyClass2", 20);//exactly the same as before
 	uniquePointer4 = 3;//make_unique<MyClass>("MyClass2", 20);
-	printf("uniquePointer4@%p\n", &uniquePointer4);
-	unique_ptr<MyClass> uniquePointer5(unique_ptr<MyClass>(new MyClass("MyClass3", 30)));
+	printf("uniquePointer4 @%p\n", &uniquePointer4);
+	unique_ptr<MyClass> uniquePointer5(unique_ptr<MyClass>(new MyClass("MyClass3", 30)));//the 3rd quivalent
 #if 0
 unique.cpp:88:50: error: use of deleted function ‘std::unique_ptr<_Tp, _Dp>::unique_ptr(const std::unique_ptr<_Tp, _Dp>&) [with _Tp = MyClass; _Dp = std::default_delete<MyClass>]’
   unique_ptr<MyClass> uniquePointer2(uniquePointer);
@@ -129,5 +173,6 @@ unique.cpp:120:39: error: invalid conversion from ‘int’ to ‘MyClass*’ [-
   unique_ptr<MyClass> uniquePointer5 = 3;//make_unique<MyClass>("MyClass2", 20);
                                        ^                                        
 #endif
+	printf("auto des of stack variable:\n");
 	return 0;
 }
