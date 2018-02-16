0faf8974635d6f8f68d3fcb8e0252e993cabb685
sliu <sz_liusha@spectratech.com>
2018-02-16 13:39:30 +0800

    public inherit+class member cons/des test

--- cpp/auto_des.cpp
+++ cpp/auto_des.cpp
@@ -16,6 +16,23 @@ public:
 		printf("%s: %p\n", __func__, this);
 	}
 };
+
+class test {
+public:
+	test(const test& src)
+	{
+		printf("%s(const test& %p): %p<=%p\n", __func__, &src, this, &src);
+	}
+	test()
+	{
+		printf("%s(): %p\n", __func__, this);
+	}
+	~test()
+	{
+		printf("%s: %p\n", __func__, this);
+	}
+};
+
 class Parser {
 public:
 	~Parser()
@@ -41,12 +58,28 @@ public:
 		printf("%s: %p\n", __func__, this);
 	}	
 };
+
+class MyClass3: public MyClass {
+	test m_obj;
+public:
+	MyClass3()
+	{
+		printf("%s(): %p\n", __func__, this);
+	}
+	~MyClass3()
+	{
+		printf("%s: %p\n", __func__, this);
+	}
+};
+
 int main()
 {
 	printf("member is class:\n");
 	Parser parser;
 	printf("public inherit:\n");
 	MyClass2 obj;
+	printf("public inherit+class member:base cons then mem cons then whole class cons\n");
+	MyClass3 obj2;
 	printf("before return:\n");
 	return 0;
 }
