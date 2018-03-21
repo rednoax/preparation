7f72b5a41e43d8e0b9aa5030ba127a6df82fee13
sliu <sz_liusha@spectratech.com>
2018-03-21 15:33:57 +0800

    test =++ and {} in array initialization

--- msm-3.18/strlen.c
+++ msm-3.18/strlen.c
@@ -12,11 +12,17 @@ struct array tags[] = {
 	ARRAY(compatible),
 	ARRAY(model),
 };
+struct array tags2[] = {
+	{}, {}, {},
+};
 #define FDT_ALIGN(x, a) (((x) + (a) -1) & ~((a) - 1))
 int main()
 {
 	int i, j;
 	int len , len_aligned;
+	unsigned int l = 0, allocl;
+	allocl = l++;
+	printf("allocl %d, l %d\n", allocl, l);
 	for (i = 0; i < ARRAY_SIZE(tags); i++) {
 		const struct array* tag = &tags[i];
 		len = 0;
@@ -33,5 +39,6 @@ int main()
 		printf("total len %d(0x%x)=>%d(0x%x)\n", len, len, len_aligned, len_aligned);
 	}
 	printf("(uint)~0ULL is %x\n", (unsigned int)~0ULL);
+	printf("%ld, %ld, each %ld\n", ARRAY_SIZE(tags2), sizeof(tags2), sizeof(struct array));
 	return 0;
 }
