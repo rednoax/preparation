e0ac8be23cc156619f61ef6a5e08dee0c9d5b0cb
sliu <sz_liusha@spectratech.com>
2018-12-29 12:34:26 +0800

    1st version for test pthread_setaffinity_np

---
+++ pthread/pthread_setaffinity.c
@@ -0,0 +1,86 @@
+#define _GNU_SOURCE
+#include <pthread.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#define CONFIG_THREAD_NR 4
+
+static err_doit(int errnoflag, int error, const char *fmt, va_list ap)
+{
+	char buf[4096];
+	vsnprintf(buf, sizeof(buf) - 1, fmt, ap);
+	if (errnoflag)
+		snprintf(buf + strlen(buf), sizeof(buf) - 1 - strlen(buf), ":%s", strerror(error));
+	strcat(buf, "\n");
+	fflush(stdout);
+	fputs(buf, stderr);
+	fflush(NULL);
+}
+
+void err_exit(int error, const char *fmt, ...)
+{
+	va_list ap = va_start(ap, fmt);
+	err_doit(1, error, fmt, ap);
+	va_end(ap);
+	exit(1);
+}
+
+static int glob = 0;
+
+struct arg {
+	int loops;
+	int cpu;
+};
+
+/*
+pthread_setaffinity_np
+http://man7.org/linux/man-pages/man3/pthread_setaffinity_np.3.html
+*/
+static void *threadFunc(void *_arg)
+{
+	struct arg * argp = _arg;
+	int loops = argp->loops;
+	int i, s, cpu = argp->cpu;
+	pthread_t thread = pthread_self();
+	
+	cpu_set_t cpuset;
+	CPU_ZERO(&cpuset);
+	CPU_SET(cpu, &cpuset);
+	s = pthread_setaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	if (s != 0)
+		err_exit(s, "***pthread_setaffinity_np for %d on CPU %d", thread, cpu);
+	s = pthread_getaffinity_np(thread, sizeof(cpu_set_t), &cpuset);
+	if (s != 0)
+		err_exit(s, "***pthread_getaffinity_np for %d on CPU %d", thread, cpu);
+	printf("Expect %d on thead %d:", cpu, thread);
+	for (i = 0; i < CONFIG_THREAD_NR; i++) {
+		if (CPU_ISSET(i, &cpuset))
+			printf("CPU %d\n", i);
+	}
+#if 0	
+	for (i = 0; i < loops; i++)
+		glob++;
+#endif
+	return NULL;
+}
+
+int main(int argc, char **argv)
+{
+	pthread_t t[CONFIG_THREAD_NR];
+	int loops = 10000000, s, i;
+	if (argc > 1)
+		loops = atoi(argv[1]);
+	printf("loop %d\n", loops);
+	arg.loops = loops;
+	for (i = 0; i < CONFIG_THREAD_NR; i++) {
+		arg.cpu = i;
+		s = pthread_create(&t[i], NULL, threadFunc, &arg);
+		if (s != 0)
+			err_exit(s, "***cannot create thread %d", i);
+		s = pthread_join(t[i], NULL);
+		if (s != 0)
+			err_exit(s, "***join %d", i);		
+	}
+	return 0;
+}
