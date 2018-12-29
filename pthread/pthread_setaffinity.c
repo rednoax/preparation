a867c9921a271eecc7264f76ddf1577e4a0aeeba
sliu <sz_liusha@spectratech.com>
2018-12-29 13:49:59 +0800

    fix 2 bugs and add sleep for start top -H -p pid
    1. fix arg transfering race bug when using a single arg
    
    2. fix serial during pthread_create/join

--- pthread/pthread_setaffinity.c
+++ pthread/pthread_setaffinity.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #define CONFIG_THREAD_NR 4
 
 static void err_doit(int errnoflag, int error, const char *fmt, va_list ap)
@@ -60,7 +61,14 @@ static void *threadFunc(void *_arg)
 		if (CPU_ISSET(i, &cpuset))
 			printf("CPU %d\n", i);
 	}
-#if 10
+#if 0
+	printf("==t%ld sleep\n", thread);
+	/*
+https://stackoverflow.com/questions/8032372/how-can-i-see-which-cpu-core-a-thread-is-running-in
+	f to select last CPU field to make sure 4 threads are on expected CPU;
+	*/
+	pause();
+#else
 	for (i = 0; i < loops; i++)
 		glob++;
 #endif
@@ -71,20 +79,48 @@ int main(int argc, char **argv)
 {
 	pthread_t t[CONFIG_THREAD_NR];
 	int loops = 10000000, s, i;
-	struct arg arg;
+	pthread_t thread = pthread_self();
+	/*
+	if using single arg rather than array, RACE will occure as following!!!:
+loop 10000000
+create 139728456488704, expect on CPU 0
+create 139728448096000, expect on CPU 1
+create 139728439703296, expect on CPU 2
+create 139728431310592, expect on CPU 3
+Expect 3 on thead 139728431310592:CPU 3
+==t139728431310592 sleep
+Expect 3 on thead 139728439703296:CPU 3
+==t139728439703296 sleep
+Expect 2 on thead 139728448096000:CPU 2
+==t139728448096000 sleep
+Expect 1 on thead 139728456488704:CPU 1
+==t139728456488704 sleep
+	*/
+	struct arg arg[CONFIG_THREAD_NR];
 
 	if (argc > 1)
 		loops = atoi(argv[1]);
-	printf("loop %d\n", loops);
-	arg.loops = loops;
+	printf("%d loop %d\n", getpid(), loops);
+	sleep(10);
 	for (i = 0; i < CONFIG_THREAD_NR; i++) {
-		arg.cpu = i;
-		s = pthread_create(&t[i], NULL, threadFunc, &arg);
+		struct arg * argp = arg + i;
+		argp->loops = loops;
+		argp->cpu = i;
+		s = pthread_create(&t[i], NULL, threadFunc, argp);
 		if (s != 0)
 			err_exit(s, "***cannot create thread %d", i);
+		else
+			printf("create %ld, expect on CPU %d\n", t[i], i);
+	}
+	for (i = 0; i < CONFIG_THREAD_NR; i++) {
 		s = pthread_join(t[i], NULL);
 		if (s != 0)
 			err_exit(s, "***join %d", i);		
 	}
+	/*
+	printf("t%ld sleep\n", thread);
+	pause();
+	*/
+	printf("Final %d\n", glob);
 	return 0;
 }
