8c9ad18b54eb9606197b4ea977b7f072eb23a85d
sliu <sz_liusha@spectratech.com>
2019-01-06 18:51:57 +0800

    add one more unlock test

--- pthread/smp_mb.c
+++ pthread/smp_mb.c
@@ -1089,6 +1089,27 @@ int unlock_with_inc_dec_nb_v1(struct arg *argp)
 }
 
 
+int unlock_with_inc_dec_nb_v2(struct arg *argp)
+{
+	int local = UNLOCKED;
+	__asm__ __volatile__(
+"	nop\n"
+"	ldr %1, [%2]\n"
+"	add %1, %1, #0xff\n"
+"	str %1, [%2]\n"
+"	nop\n"
+"	ldr %1, [%2]\n"
+"	sub %1, %1, #0xff\n"
+"	str %1, [%2]\n"
+"	nop\n"
+"	str %4, [%0]\n"
+"	nop\n"
+	:
+	: "r" (&my_lock), "r"(UNLOCKED), "r" (glob), "I"(UNLOCKED), "r"(local)
+	: "cc");
+	return 0;
+}
+
 int unlock_with_inc_dec_nb(struct arg *argp)
 {
 	glob[0]++;
@@ -1168,6 +1189,7 @@ mutex mutexes[][2] = {
 	//{spin_lock_more_more_simple_bl_nb, unlock_with_inc_dec_nb},//***25096(0.000627% 39974904<40000000)
 	//{spin_lock_more_more_simple_bl_nb, unlock_with_inc_dec_nb_v0}//***398547(0.009964% 39601453<40000000)
 	{spin_lock_more_more_simple_bl_nb, unlock_with_inc_dec_nb_v1},//
+	{spin_lock_more_more_simple_bl_nb, unlock_with_inc_dec_nb_v2},//
 	//{spin_lock_more_more_simple_bl_nb_v0, unlock_with_nop_nb},//**265650(0.006641% 39734350<40000000)
 	//{spin_lock_more_more_simple_bl_nb_v1, unlock_with_nop_nb}//***673290(0.016832% 39326710<40000000)
 	//{spin_lock_more_more_simple_nb, unlock_with_nop_nb},//***209601(0.005240% 39790399<40000000)
