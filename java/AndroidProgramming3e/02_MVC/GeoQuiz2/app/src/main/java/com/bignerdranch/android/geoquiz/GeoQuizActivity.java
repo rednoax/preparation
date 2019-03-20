2b8efdbc0f145e75a7db7d685816f55ae1d3344a
sliu <sz_liusha@spectratech.com>
2019-03-20 16:34:02 +0800

    My version of chapter 2's challenge
    There is bug in updateQuestion in onCreate

--- java/AndroidProgramming3e/02_MVC/GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz/GeoQuizActivity.java
+++ java/AndroidProgramming3e/02_MVC/GeoQuiz2/app/src/main/java/com/bignerdranch/android/geoquiz/GeoQuizActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ImageButton;
 import android.view.View;
+import android.util.Log;
 
 public class GeoQuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private Button[] mPrevButton = new Button[2];
+    private Button[] mNextButton = new Button[2];
+    private ImageButton mPrevImageButton;
+    private ImageButton mNextImageButton;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;//verbose assignment
     private Question[] mQuestionBank = new Question[] {
@@ -27,9 +33,14 @@ public class GeoQuizActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mTrueButton = (Button)findViewById(R.id.true_button);
         mFalseButton = (Button)findViewById(R.id.false_button);
-        mNextButton = (Button)findViewById(R.id.next_button);
+        mPrevButton[0] = (Button)findViewById(R.id.prev_button);
+        mPrevButton[1] = (Button)findViewById(R.id.prev_button2);
+        mPrevImageButton = (ImageButton)findViewById(R.id.prev_button3);
+        mNextButton[0] = (Button)findViewById(R.id.next_button);
+        mNextButton[1] = (Button)findViewById(R.id.next_button2);
+        mNextImageButton = (ImageButton)findViewById(R.id.next_button3);
         mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
-        updateQuestion();
+        updateQuestion(true);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -42,17 +53,54 @@ public class GeoQuizActivity extends AppCompatActivity {
                 checkAnswer(false);
             }
         });
-        mNextButton.setOnClickListener(new View.OnClickListener() {
+        for (int i = 0; i < mNextButton.length; i++) {
+            mPrevButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateQuestion(false);
+                }
+            });
+            mNextButton[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    updateQuestion(true);//mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+                }
+            });
+        }
+        mPrevImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
-                updateQuestion();//mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getTextResId());
+                updateQuestion(false);
+            }
+        });
+        mNextImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(true);
+            }
+        });
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateQuestion(true);
             }
         });
     }
-    private void updateQuestion() {
+    private void updateQuestion(boolean inc) {
+        int oldIndex = mCurrentIndex;
+        if (inc)
+            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+        else {
+            mCurrentIndex -= 1;
+            if (mCurrentIndex < 0)
+                mCurrentIndex = mQuestionBank.length - 1;
+        }
+        String string = String.format("%d=>%d%n", oldIndex, mCurrentIndex);
+        Log.v(TAG, string);
         int question = mQuestionBank[mCurrentIndex].getTextResId();
-        mQuestionTextView.setText(question);
+        string = String.format("[%d=>%d]%s", oldIndex, mCurrentIndex, getResources().getString(question));
+        //mQuestionTextView.setText(question);
+        mQuestionTextView.setText(string);
     }
     private void checkAnswer(boolean userPressedTrue) {
         int messageResId;
