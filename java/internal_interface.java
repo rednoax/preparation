4191f6608112000e0f5576d66d95081fc0f3f5fe
sliu <sz_liusha@spectratech.com>
2019-04-25 12:39:05 +0800

    strengthen that outer class name cannot be ignored when accessing inner interface

--- java/internal_interface.java
+++ java/internal_interface.java
@@ -10,12 +10,13 @@ final class ViewTreeObserver {
     https://beginnersbook.com/2016/03/nested-or-inner-interfaces-in-java/
     An interface i.e. declared inside the interface or class, is known as nested
     interface. 
-    1. It is static by default. You don¡¯t have to mark them static explicitly as it would be redundant.
+    1. It is static by default. You don't have to mark them static explicitly as it would be redundant.
     2. Since nested interface cannot be accessed directly, the main purpose of using
        them is to resolve the namespace by grouping related interfaces (or related
        interface and class) together.
     3. This way, we can only call the nested interface by using outer class or outer
-       interface name followed by dot( . ), followed by the interface name.
+       interface name followed by dot( . ), followed by the interface name. If no outer class name and
+       only inner interface name is used, build error emit: "cannot find symbol"
     4. Nested interfaces declared inside class can take any access modifier, however
        nested interface declared inside interface is public implicitly.
     */
@@ -35,7 +36,12 @@ class TextView implements ViewTreeObserver.onPreDrawListener {
  * @author Administrator
  */
 public class internal_interface {
-
+    /*
+    build error:cannot find symbol
+    public void set(onPreDrawListener listener)
+    */
+    public void set(ViewTreeObserver.onPreDrawListener listener) {
+    }
     /**
      * @param args the command line arguments
      */
