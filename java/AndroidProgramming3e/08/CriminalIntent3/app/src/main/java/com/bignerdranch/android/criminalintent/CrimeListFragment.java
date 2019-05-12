212734e3867ab76d58ad2d1d835fc033fa293475
sliu <sz_liusha@spectratech.com>
2019-05-12 12:23:49 +0800

    add 08 code

---
+++ java/AndroidProgramming3e/08/CriminalIntent3/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -0,0 +1,162 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import static java.lang.String.valueOf;
+
+public class CrimeListFragment extends Fragment {
+    private final static String TAG = "CrimeListFragment";
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+    private void dump(String s) {
+        String fmt = String.format("%s:%s", valueOf(this), s);
+        Log.d(TAG, fmt);
+    }
+    public CrimeListFragment() {
+        super();
+        String fmt = String.format("cons: Activity %s", getActivity2());
+        dump(fmt);
+    }
+    FragmentActivity getActivity2() {
+        FragmentActivity fa = getActivity();
+        dump(String.format("get2:%s", fa));
+        return fa;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView = (RecyclerView)view.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity2()));
+        updateUI();
+        String fmt = String.format("onCreateView(,%s,%s)%n" +
+                "%s==%s:%b%n" +
+                "getActivity:%s%n" +
+                "LayoutInflater:%s%n",
+                valueOf(container), valueOf(savedInstanceState),
+                valueOf(view), valueOf(mCrimeRecyclerView), view == (View)mCrimeRecyclerView,
+                valueOf(getActivity2()),
+                String.valueOf(LayoutInflater.from(getActivity2())));
+        dump(fmt);
+        return view;
+    }
+    private void updateUI() {
+        CrimeLab crimeLab = CrimeLab.get(getActivity2());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter = new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+    }
+    private class CrimeHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private final static String TAG = "CrimeHolder";
+        private Crime mCrime;
+        private ViewGroup mViewGroup;//==itemView ALWAYS
+        private TextView mTitleTextView;
+        private TextView mDateTextView;
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            String fmt = String.format("%x onClick(%s): Activity:%s, @%d", this.hashCode(), String.valueOf(v), String.valueOf(getActivity2()), position);
+            Toast.makeText(getActivity2(), mCrime.getTitle() + " clicked!", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, fmt);
+            if (position == 0) {
+                /*
+                1. click 0, toast show "crime#0 clicked", then position 0&1 will swap: position 0 holds "crime#1" and position 1 holds "crime#0"
+                2. after 1's swap, then click "crime#1" that is now at position 0, toast show "crime#1 clicked", then position 0&1 will swap. So
+                at last it becomes to the original state before 1
+                * */
+                mAdapter.notifyItemMoved(position, position + 1);
+            }
+        }
+
+        public CrimeHolder(LayoutInflater layoutInflater, ViewGroup parent) {
+            super(layoutInflater.inflate(R.layout.list_item_crime, parent ,false));
+            mViewGroup = itemView.findViewById(R.id.crime_linear_layout);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.crime_title);
+            mDateTextView = (TextView) itemView.findViewById(R.id.crime_date);
+            itemView.setOnClickListener(this);
+            String fmt = String.format("%nCrimeHolder(,%b):%x%n" +
+                    "itemView:%s%n" +
+                    "inflate return==root ViewGroup:%b%n",
+                    parent == mCrimeRecyclerView, this.hashCode(),
+                    String.valueOf(itemView),
+                    itemView == (View)mViewGroup);
+            dump(fmt);
+        }
+        public void bind(Crime crime) {
+            mCrime = crime;
+            mTitleTextView.setText(mCrime.getTitle());
+            mDateTextView.setText(mCrime.getDate().toString());
+        }
+    }
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder> {
+        private final static String TAG = "CrimeAdapter";
+        private List<Crime> mCrimes;
+        public CrimeAdapter(List<Crime> crimes) {
+            mCrimes = crimes;
+        }
+        private int mGetItemViewTypeTimes = 0;
+        private int mLastPostion = -1;
+        private int mOnBindViewHolderTimes = 0;
+        public void recGetTimes(int position) {
+            mGetItemViewTypeTimes++;
+            mLastPostion = position;
+        }
+        public void recBindTimes(int position) {
+            String s = "";
+            if (mGetItemViewTypeTimes > mOnBindViewHolderTimes) {
+                mOnBindViewHolderTimes++;
+                if (position != mLastPostion) {
+                    s ="***";
+                }
+                /*From debug:
+                getItemViewType is called much more times than onBindViewHolder! However, onBindViewHolder(,position)
+                will be always preceded with a call of getItemViewType(position). And their position are CERTAINLY the same!
+                */
+                s = String.format("%s%d==%d? @%d", s, mGetItemViewTypeTimes, mOnBindViewHolderTimes, mLastPostion);
+                Log.d(TAG, s);
+            }
+        }
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity2());
+            String fmt = String.format("Adapter:onCreateViewHolder(0x%x,%d): LayoutInflater:%s",
+                    parent.hashCode(), viewType, String.valueOf(layoutInflater));
+            dump(fmt);
+            return new CrimeHolder(layoutInflater, parent);
+        }
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+            Crime crime = mCrimes.get(position);
+            String fmt = String.format("Adapter:onBind(%x, %d)", holder.hashCode(), position);
+            dump(fmt);
+            holder.bind(crime);
+            recBindTimes(position);
+        }
+        @Override
+        public int getItemCount() {
+            dump("Adapter:getItemCount()");
+            return mCrimes.size();
+        }
+        @Override
+        public int getItemViewType(int position) {
+            int ret = super.getItemViewType(position);
+            String fmt = String.format("Adapter:getItemViewType(%d): %d", position, ret);
+            dump(fmt);
+            recGetTimes(position);
+            return ret;
+        }
+    }
+
+}
