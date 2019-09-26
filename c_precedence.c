0068de75e4040e2db48f750627a08ef118dc271c
sliu <sz_liusha@spectratech.com>
2019-09-26 18:08:44 +0800

    test *(p++)=SomeValue

--- c_precedence.c
+++ c_precedence.c
@@ -15,5 +15,8 @@ int main()
 	p = addr;
 	tmp = *p++;
 	printf("*p++\t:addr %p, p %p, tmp 0x%lx\n", addr, p, tmp);
+	p = addr;
+	*(p++) = 0;
+	printf("*(p++)=0\t:addr %p, %p, addr[]:%lx %lx\n", addr, p, addr[0], addr[1]);
 	return 0;
 }
