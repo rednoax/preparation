c9a684e2f741bce7452e543e340d990952d10fcb
sliu <sz_liusha@spectratech.com>
2020-01-10 21:09:18 +0800

    fix build error and add debug result

--- cpp/obj_in_func3.cpp
+++ cpp/obj_in_func3.cpp
@@ -7,7 +7,7 @@ class sp
 public:
 	sp(): m_ptr(0) {report_func();}
 	sp(const sp<T>& other): m_ptr(other.m_ptr) {report_func();}
-	sp(T* other): m_ptr(other) {report_func();}
+	sp(T* other): m_ptr(other) {report_func();printf("%p\n", other);}
 	~sp() {report_func();}
 	T* get() const {report_func();return m_ptr;}
 	sp& operator=(T*other)
@@ -20,6 +20,11 @@ public:
 	{
 		report_func();
 		return m_ptr != o;
+	}
+	T* operator->() const
+	{
+		report_func();
+		return m_ptr;
 	}
 private:
 	T *m_ptr;
@@ -67,16 +72,17 @@ class IServiceManager: public IInterface
 public:
 	static const String descriptor;
 	static const String descriptor2;
-	static sp<IInterface> asInterface(const sp<IBinder>&obj)
+	static sp<IServiceManager> asInterface(const sp<IBinder>&obj)
 	{
-		report_line();
+		report_line("\n");
 		sp<IServiceManager> intr;
 		if (obj != NULL) {
 			intr = static_cast<IServiceManager*>(
 				obj->queryLocalInterface(
-					IServiceManager::descriptor).get());
-			printf("intr %p\n", intr);
+					IServiceManager::descriptor).get());//at last .get() return "IInterface*"
+			printf("intr %p\n", intr.get());
 		}
+		return NULL;
 	}
 	IServiceManager() {report_func();}
 	~IServiceManager() {report_func();}
@@ -105,7 +111,39 @@ sp<IBinder> getContexObject(const sp<IBinder>&)
 
 int main()
 {
-	report_line();
+	report_line("\n");
 	interface_cast<IServiceManager>(getContexObject(NULL));
 	return 0;
 }
+/*
+test result:
+String::String(const char*):0x55e8e81fd019<--static var is like global var, their ctors run 1st
+String::String(const char*):0x55e8e81fd01a<--descriptor("...") is equivalent to descriptor2 = "..."
+int main() 114:
+sp<T>::sp(T*) [with T = IBinder]:0x7fffac0f4050<--NULL to a temp sp<IBinder> object, whose dtor is not called before main's return 0, even the argument object is not used  at all
+(nil)
+sp<T>::sp() [with T = IBinder]:0x7fffac0f4058<--stack temp obj @result's ctor
+IBinder::IBinder():0x55e8e8f38e80<---new BpBinder will construct base class IBinder 1st
+BpBinder::BpBinder(int):0x55e8e8f38e80<---then derived class ctor, whose this == the above line
+sp<T>& sp<T>::operator=(T*) [with T = IBinder]:0x7fffac0f4058<--result = b; this==@result's this, the stack var @result is used so it will not be destructed until it is not used any more
+static sp<IServiceManager> IServiceManager::asInterface(const sp<IBinder>&) 77:
+sp<T>::sp() [with T = IServiceManager]:0x7fffac0f3fe8<---sp<IServiceManager> intr;'s ctor
+bool sp<T>::operator!=(const T*) const [with T = IBinder]:0x7fffac0f4058<---!='s this is the stack object of the above @result
+T* sp<T>::operator->() const [with T = IBinder]:0x7fffac0f4058<---stack var's ->
+virtual sp<IInterface> IBinder::queryLocalInterface(const String&):0x55e8e8f38e80<--IBinder's virtual func, whose this==BpBinder's
+sp<T>::sp(T*) [with T = IInterface]:0x7fffac0f3ff0<--a temp object sp<IInterface> is constructed with NULL when queryLocalInterface returned
+(nil)
+T* sp<T>::get() const [with T = IInterface]:0x7fffac0f3ff0<--the temp object of the above line is used to call .get()
+sp<T>& sp<T>::operator=(T*) [with T = IServiceManager]:0x7fffac0f3fe8<--intr=static_cast<IServiceManager*>(...), whose this==@intr's
+sp<T>::~sp() [with T = IInterface]:0x7fffac0f3ff0<---the returned temp object's dtor, since it is not used after supplying .get() with this
+T* sp<T>::get() const [with T = IServiceManager]:0x7fffac0f3fe8<--intr.get()
+intr (nil)
+sp<T>::sp(T*) [with T = IServiceManager]:0x7fffac0f4060<--a temp object is constructed for asInterface returns NULL
+(nil)
+sp<T>::~sp() [with T = IServiceManager]:0x7fffac0f3fe8<--intr's dtor
+sp<T>::~sp() [with T = IServiceManager]:0x7fffac0f4060<--the returned object by asInterface is not used so it is destructed at once
+sp<T>::~sp() [with T = IBinder]:0x7fffac0f4058<--stack var returned by getContexObject is not used and released in mirror with the below
+sp<T>::~sp() [with T = IBinder]:0x7fffac0f4050<---NULL to a temp sp<IBinder> object's dtor
+String::~String():0x55e8e81fd01a<---static var's dtor runs in mirror
+String::~String():0x55e8e81fd019<---ditto
+*/
