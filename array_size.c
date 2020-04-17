732cdbf5c6db81df318d47b22e0c84704b49cd17
sliu <sz_liusha@spectratech.com>
2020-04-17 15:47:39 +0800

    debug for all zero array

--- array_size.c
+++ array_size.c
@@ -4,16 +4,25 @@ struct of_dev_auxdata {
 	char *compatible;
 };
 
-static struct of_dev_auxdata msm8909_auxdata_lookup[] = {
+static struct of_dev_auxdata msm8909_auxdata_lookup[] = {//ARRAY_SIZE:1, all zero, confirmed by dump
 	{},
 };
 
-static struct of_dev_auxdata msm8909_auxdata_lookup2[] = {
+static struct of_dev_auxdata msm8909_auxdata_lookup2[] = {//ARRAY_SIZE:2, all zero, confirmed by dump
 	{},{}
 };
+
+void dump(struct of_dev_auxdata *p, int array_size)
+{
+	int i;
+	for (i = 0; i < array_size; i++)
+		printf("[%d]: %p\n", i, p[i].compatible);
+}
 
 int main(int argc, char *argv[])
 {
 	printf("%ld %ld\n", ARRAY_SIZE(msm8909_auxdata_lookup), ARRAY_SIZE(msm8909_auxdata_lookup2));
+	dump(msm8909_auxdata_lookup, ARRAY_SIZE(msm8909_auxdata_lookup));
+	dump(msm8909_auxdata_lookup2, ARRAY_SIZE(msm8909_auxdata_lookup2));
 	return 0;
 }
