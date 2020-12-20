8f1297fa98dc152d0141c0aadb118245076832df
rednoah <rednoax@gmail.com>
2020-12-20 09:20:37 +0800

    add proxy for nico.sh

--- bash/nico.sh
+++ bash/nico.sh
@@ -1,15 +1,18 @@
+# youtube-dl --proxy socks5://192.168.12.254:1081/ -f best https://www.nicovideo.jp/watch/sm37978391
+EXE="youtube-dl --proxy socks5://192.168.12.254:1081/"
+
 tmp=__tmpfile
 files=$*
 declare -i count r1
 for file in $files
 do
-	youtube-dl -F $file
+	$EXE -F $file
 	r1=1
 	count=1
 	while (( $r1 != 0 ))
 	do
 		echo "###count $count"
-		r0=`youtube-dl -f best $file`
+		r0=`$EXE -f best $file`
 		echo "$r0" | tee $tmp
 		echo -e "###\t$count fin\n"
 		grep 100% $tmp
