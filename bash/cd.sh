7ad6010b1f8a915998fbd3d6033f761333973c43
rednoah <rednoax@gmail.com>
2020-12-22 13:31:38 +0800

    source cd.sh test

---
+++ bash/cd.sh
@@ -0,0 +1,6 @@
+cd ~
+#1. if run in shell like ./cd.sh, when script finishes, the bash 's PWD is not changed
+
+#2. if source cd.sh, when script finishes, the bash will goto ~/
+#rednoah@lucia:~/preparation/bash$ source cd.sh
+#rednoah@lucia:~$
