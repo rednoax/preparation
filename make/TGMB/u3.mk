82597eab80bca3fe664a8e96e236ef05c15b4e88
rednoah <rednoax@gmail.com>
2020-12-22 11:14:29 +0800

    add one test result

--- make/TGMB/u3.mk
+++ make/TGMB/u3.mk
@@ -17,16 +17,19 @@ all2 all3 all4:
 all3:FOO=all3
 all4:override FOO=all4
 
-#make -f u3.mk all0 all1 all2 all3 all4
+#$ make -f u3.mk all0 all1 all2 all3 all4
 #all0
 #<--empty, exported and cmd arg will all becomes envp of shell process!unexported var in file will never be seen in shell process
 #file
 #all3<--as all3 has override FOO as all3
 #all4<--file var with override takes precedence to command line var
 
-#make -f u3.mk all0 all1 all2 all3 all4 FOO=arg
+#$ make -f u3.mk all0 all1 all2 all3 all4 FOO=arg
 #arg<---var from cmd line can be seen in shell process's env by the 3rd argument of int main(,,char **envp)
 #arg
 #arg
 #arg
 #all4<--file var with override takes precedence to command line var
+
+#$ FOO=1 make -f u3.mk all1
+#all1<--FOO is env inherited by shell process that runs "echo $FOO"
