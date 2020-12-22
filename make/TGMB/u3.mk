19f20b4acc0f7e30be80b150c8520b8da53531f5
rednoah <rednoax@gmail.com>
2020-12-22 10:11:20 +0800

    add file var with override

--- make/TGMB/u3.mk
+++ make/TGMB/u3.mk
@@ -1,21 +1,32 @@
+#TO TEST:
+#target specific variable
+#target specific exported var
+
 FOO=file
 all0 all1:
 	@echo $$FOO
+#the above will be transfered to shell as "echo $FOO"
+
 all0:export FOO=all0
-all1:FOO=all1
+all1:FOO=all1#
 
-all2 all3:
+all2 all3 all4:
 	@echo $(FOO)
-all3:FOO=all3
+#the above $(FOO) will be replaced by make then transfered to shell
 
-#make all0 all1 all2 all3
+all3:FOO=all3
+all4:override FOO=all4
+
+#make -f u3.mk all0 all1 all2 all3 all4
 #all0
 #<--empty, exported and cmd arg will all becomes envp of shell process!unexported var in file will never be seen in shell process
 #file
 #all3<--as all3 has override FOO as all3
+#all4<--file var with override takes precedence to command line var
 
-#make all0 all1 all2 all3 FOO=arg
+#make -f u3.mk all0 all1 all2 all3 all4 FOO=arg
 #arg<---var from cmd line can be seen in shell process's env by the 3rd argument of int main(,,char **envp)
 #arg
 #arg
 #arg
+#all4<--file var with override takes precedence to command line var
