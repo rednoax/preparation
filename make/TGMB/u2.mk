7299e9030d2c5b001174babb73d434dda4a60a07
rednoah <rednoax@gmail.com>
2020-12-22 09:36:04 +0800

    Add env defination before elf

--- make/TGMB/u2.mk
+++ make/TGMB/u2.mk
@@ -1,3 +1,6 @@
+#TO TEST:
+#a c program can dump all its args/envs to verify the cmd line var/file var/env var of makefile
+
 VAR3=file
 
 ifeq (1,1)
@@ -16,15 +19,17 @@ export VAR1=1 VAR4=file4
 #36:32: VAR4=4
 endif
 
-all:;@./fakemake.out VAR2=2
+#VAR5=5 will be env while VAR2=2 is argument
+all:;@VAR5=5 ./fakemake.out VAR2=2
 
-#note there is no VAR3 among the following output, if a var is nither exported nor defined at cmd line, it will not be seen in
-#shell process's env/arg.
+#note there is no VAR3 among the following output, if a var is nither exported nor defined at cmd line, it will nither be seen in
+#shell process's env nor args.
 
 #rednoah@lucia:~/preparation/make/TGMB$ make -f u2.mk VAR0=0 VAR4=4 | grep -n VAR
 #2:1: VAR2=2<--VAR2 is arg, will not be shown in env
 #3:VVVVARVVV<--separator, the above is args and the below is env
-#22:18: VAR0=0<--command line arg becomes an env
-#29:25: VAR1=1<--exported var becomes an env
-#32:28: MAKEFLAGS= -- VAR4=4 VAR0=0<--the flags from cmd line
-#36:32: VAR4=4<--if one var is both from cmd line and file's exported var, then cmd line takes precedence.
+#15:11: MAKEFLAGS= -- VAR4=4 VAR0=0<--the flags from cmd line
+#21:17: VAR0=0<--command line arg becomes an env
+#22:18: VAR1=1<--exported var becomes an env
+#28:24: VAR4=4<--if one var is both from cmd line and file's exported var, then cmd line takes precedence.
+#31:27: VAR5=5<--the var definination before elf is env!
