ae0f40e15b0ebe23b687ebd6862f63d0ccec0c98
rednoax <rednoax@gmail.com>
2021-01-21 20:05:43 -0800

    c2.4

---
+++ eos2020/ch2/c2.4/ts.s
@@ -0,0 +1,18 @@
+/* 
+.extern is accepted in the source program-for compatible with other assemblers-but
+it is ignored. as treats all undefined symbols as external!
+*/
+    .text
+.global start
+start:
+    ldr sp, =stack_top
+    ldr r4, =a
+    ldr r0, [r4], #4
+    ldr r1, [r4], #4
+    bl sum
+    str r0, [r4]
+stop: b stop
+    .data
+a: .word 0xa5
+b: .word 0x5a00
+c: .word 0
