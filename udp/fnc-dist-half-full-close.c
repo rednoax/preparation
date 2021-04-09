adb5a729d52a46555af7cb4aff461a1b00b45c4d
rednoax <rednoax@gmail.com>
2021-04-09 03:35:35 -0700

    use a single func to handle stdin/connected socket

--- udp/fnc-dist-half-full-close.c
+++ udp/fnc-dist-half-full-close.c
@@ -10,6 +10,12 @@
 #include <poll.h>
 #include <sys/types.h>
 #include <signal.h>
+#define CMP(a, b) strncmp(a, b, strlen(b))
+enum {
+STDIN,
+CONNECTED,
+LISTEN,
+};
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 int bflag, kflag, lflag, uflag;
@@ -128,7 +134,7 @@ void set_common_sockopts(int s, struct sockaddr *sa)
 	if (bflag) {
 		if (setsockopt(s, SOL_SOCKET, SO_BROADCAST, &x, sizeof(x)) == -1)//if err, the return value needs not to be reserved as err() 's err info is strerror(errno) whose arg is global var!
 			err(1, "set SO_REUSEADDR error");
-	}		
+	}
 }
 /*eg:
 $ ./a.out -nl 1080
@@ -254,7 +260,7 @@ The errx() and warnx() functions do not append an error message.
 	if (!uflag && s != -1) {//if bind err, don't listen
 		if (listen(s, 1) == -1)//for tcp, `netstat -nap` can see 'tcp 0 0 0.0.0.0:1080 0.0.0.0:* LISTEN 4201/./a.out' after listen return ok
 			err(1, "listen err");
-	}		
+	}
 	if (s != -1) {
 		struct sockaddr addr;
 		socklen_t len = sizeof(addr);
@@ -304,208 +310,148 @@ void pipehandler(int s)
 	fflush(stdout);
 }
 
-void my_poll(int s)
+void handle(struct pollfd fds[], int nr, int *flag)
+{
+	char buf[4*1024*1024];
+	int *fd = &fds[CONNECTED].fd;
+	short rev = fds[CONNECTED].revents;
+	int ret;
+	if (rev & POLLNVAL) {
+		printf("***POLLNVAL fd %d removed from poll\n", *fd);
+		*fd = -1;//must remove it from polled list, POLLNVAL emits continuously otherwise
+	}
+	if (rev & POLLIN) {
+		ret = recv(*fd, buf, sizeof(buf), 0);
+		if (ret > 0)
+			write(STDIN_FILENO, buf, ret);
+		else if (ret < 0)
+			printf("***recv %d\n", ret);
+		else if (!ret && *flag == 0) {
+			*flag = 1;//POLLIN is not removed so use it to stop poll() returing recv() 0 continuously from POLLIN
+			printf("EOF\n");
+		}
+	}
+	if (rev & POLLHUP) {
+		printf(buf, "POLLHUP\n");
+		close(*fd);
+		*fd = -1;
+	}
+	if (fds[STDIN].revents & POLLIN) {
+		ret = read(STDIN_FILENO, buf, sizeof(buf));
+		//printf("read %d\n", ret);
+		if (ret > 0 && *fd >= 0) {
+			int r;
+			if (!CMP(buf, "sdr")) {//strncmp's n should not be ret as ret contains '\n'
+	//c=>s no any packet sent from c to s after the read's shutdown
+				if ((r = shutdown(*fd, SHUT_RD)) == -1)
+					warn("sdr error");
+				else
+					printf("sdr %d\n", r);
+			} else if (!CMP(buf, "sdw")) {
+	/*c=>s [FIN,ACK];
+	s=>c [ACK]; if server ^c then, POLLHUP is got*/
+				if ((r = shutdown(*fd, SHUT_WR)) == -1)
+					warn("sdw error");
+				else
+					printf("sdw %d\n", r);
+			} else if (!CMP(buf, "close")) {
+	/*c=>s [FIN,ACK];
+	s=>c [ACK]; if server ^c then, No ANY POLLHUP can be got by client*/
+				r = close(*fd);
+				printf("close %d\n", r);
+				*fd = -1;
+			} else if (!CMP(buf, "CLOSE")) {//no POLLHUP after its close()
+				r = close(*fd);
+				fds[CONNECTED].revents = -1;//it will be written by POLLNVAL in next poll()
+				printf("CLOSE %d\n", r);
+			} else {
+				int f = 0;
+				struct sigaction sa = {
+					.sa_flags = 0,
+					.sa_handler = pipehandler,
+				};
+				if (!CMP(buf, "NOSIG")) {
+					f = MSG_NOSIGNAL;
+				} else if (!CMP(buf, "R_SIG")) {
+					if (sigaction(SIGPIPE, &sa, NULL) == -1)
+						err(1, "SIGPIPE reg err");
+				} else if (!CMP(buf, "IGN")) {
+					sa.sa_handler = SIG_IGN;
+					if (sigaction(SIGPIPE, &sa, NULL) == -1)
+						err(1, "SIGPIPE reg err");
+				}
+				r = send(*fd, buf, ret, f);
+				if (r < 0)
+					warn("***send %d, errno %d", r, errno);
+				/*else
+					warnx("send %d", r);*/
+			}
+		}
+	}
+	fflush(stdout);
+	fflush(stderr);
+}
+
+void spoll(int s)
 {
 	struct pollfd fds[3] = {
-		[0] = {
-			.fd = s,
-			.events = POLLIN,
-		},
-		[1] = {
+		[STDIN] = {
 			.fd = STDIN_FILENO,
 			.events = POLLIN,
 		},
-		[2] = {
+		[CONNECTED] = {
 			.fd = -1,
-		}
+		},
+		[LISTEN] = {
+			.fd = s,
+			.events = POLLIN,
+		},
 	};
 	int r;
+	static int flag;
 	while (1) {
 		r = poll(fds, ARRAY_SIZE(fds), -1);
 		debug_poll(r, fds, ARRAY_SIZE(fds));
-		while (r > 0) {
-			if (fds[0].revents & POLLIN) {//server
+		if (r > 0) {
+			struct pollfd *fdp = &fds[LISTEN];
+			if (fdp->revents & POLLIN) {//server
 				struct sockaddr sa;
 				struct sockaddr_in *sap = (struct sockaddr_in*)&sa;
 				socklen_t len = sizeof(sa);
-				int c = accept(fds[0].fd, &sa, &len);
+				int c = accept(fdp->fd, &sa, &len);
 				if (c > 0) {
 					char peer[INET_ADDRSTRLEN];
-					fds[2].fd = c;
-					fds[2].events = POLLIN | POLLOUT;//no need to set POLLHUP, as long as local fds[2].fd is not closed, POLLOUT will continously returned in .revents even peer(client) `sdw` or `close` to sent a FIN
+					fds[CONNECTED].fd = c;
+					fds[CONNECTED].events = POLLIN | POLLOUT;//no need to set POLLHUP, as long as local .fd connected with peer is not closed, POLLOUT will continously returned in .revents even peer(client) `sdw` or `close` to sent a FIN
 					printf("peer %s:%d\n", inet_ntop(AF_INET, &sap->sin_addr, peer, INET_ADDRSTRLEN),
 						ntohs(sap->sin_port));
 					fflush(stdout);
 				}
-				r--;
 			}
-			if (fds[1].revents & POLLIN) {//stdin
-				int ret, flags = 0, c = fds[2].fd;
-				char buf[4096];
-				ret = read(fds[1].fd, buf, sizeof(buf));
-/*
-1.c:`sdw`
-  s:send() any input from stdin is ok and client can get it normally
-2.c:`close`
-  s:send() return ok: eg send() 'bye\n' return 4, wireshark can see the packet from s to c, but client return
-  immediately one 'RST' packet to server. So poll() return at once and its client socket .revent becomes
-  0x1d(POLLHUP|POLLERR|POLLOUT|POLLIN) from old status 0x5(POLLIN|POLLOUT).	NO SIGPIPE in the process.
-  BUT, if going on to call send() with one of the following, There is no any packet between c and s can be seen by wireshark.
-1)"NOSIG\n": send() return -1, errno is EPIPE(32) and then next poll() return at once 0x15(POLLIN|POLLOUT|POLLHUP) in
-  client socket .revent. The error info is:  'a.out: ***send -1, errno 32: Broken pipe'
-2)"R_SIG\n": the same as 1. Besides, registered SIGPIPE handler is called before send() return -1
-3)"IGN": the same as 1.
-4)other ordinary input not among 1~3:program exits directly as it is killed by SIGPIPE.
-*/
-				if (ret > 0 && c >= 0) {
-					int w;
-					if (!strncmp(buf, "NOSIG", 5)) {
-						flags = MSG_NOSIGNAL;
-					} else if (!strncmp(buf, "R_SIG", 5)) {
-						struct sigaction sa = {
-							.sa_flags = 0,
-							.sa_handler = pipehandler,
-						};
-						if (sigaction(SIGPIPE, &sa, NULL) == -1)
-							err(1, "SIGPIPE reg err");
-					} else if (!strncmp(buf, "IGN", 3)) {
-						struct sigaction sa = {
-							.sa_flags = 0,
-							.sa_handler = SIG_IGN,
-						};
-						if (sigaction(SIGPIPE, &sa, NULL) == -1)
-							err(1, "SIGPIPE reg err");
-					} else if (!strncmp(buf, "close", 5)) {//if run it after the above 2-1/2/3, then input anything to lauch a poll() return:fds[2].revents will be 0 from old 0x15
-						close(c);
-						fds[2].fd = -1;
-						goto fin;
-					} else if (!strncmp(buf, "CLOSE", 5)) {
-						close(c);
-						goto fin;
-					} else if (!strncmp(buf, "sdw", 3)) {
-						if (shutdown(c, SHUT_WR) == -1)
-							err(1, "SHUT_WR");
-						goto fin;
-					} else if (!strncmp(buf, "sdr", 3)) {
-						w = shutdown(c, SHUT_RD);
-						warn("sdr %d", w);
-					}
-					w = send(c, buf, ret, flags);
-					if (w < 0)
-						warn("***send %d, errno %d", w, errno);
-					else
-						warnx("send %d", w);
-				}
-fin:			r--;
-			}
-//The field fd contains a file descriptor for an open file. If this field is negative, then the corresponding events field is ignored and the revents field returns zero.
-			if (fds[2].revents & (POLLIN | POLLHUP)) {//client, if .fd==-1, revents return 0 so skip .fd check befor .revents check
-				char buf[4096];
-				int ret = recv(fds[2].fd, buf, sizeof(buf), 0);
-/*recv() return:When a stream socket peer has performed an orderly shutdown, the return value will be 0
-(the traditional "end-of-file" return).
-TODO: when client ^+c:here no POLLHUP but POLLIN then recv return 0
-why no POLLHUP? stackoverflow's 'poll-cant-detect-event-when-socket-is-closed-locally' answer doesn't make sense;
-TCP/IP illustrated vol1 2nd:
-In the FIN_WAIT_2 state, TCP has sent a FIN and the other end has acknowledged<-both close() shutdown(,SH_WR) can send a FIN
-it. Unless a half-close is being performed, the TCP must wait for the application
-on the other end to recognize that it has received an end-of-file notification and<-so passive close peer get EOF.The tcp connection is just half closed now.so no POLLHUP
-close its end of the connection, which causes a FIN to be sent.<-!
-*/
-				if (ret >= 0)
-					write(STDIN_FILENO, buf, ret);
-				if (fds[2].revents & POLLIN && !ret) {//& precedes &&
-/*when the following close line opened:
-if c runs `sdw` or `close`(these 2 have the same effect to send [FIN ACK] except the former can still read
-the file descriptor but the latter cannot as fd has been invalid), a simultaneous close is observed by wireshark:
-c=>s [FIN ACK] Seq=1 Ack=1
-s=>c [FIN ACK] Seq=1 Ack=2<--Ack is last Seq's
-c=>s [ACK] Seq=2 Ack=2<--Ack is last Seq's
-TODO: this introduced a bug: if peer runs `sdw` to half-close, server finds it here but does a full close.
-The half close expected by client becomes a full close for the connection.
-*/			
-					//close(fds[2].fd);fds[2].fd = -1;//ALA this close line is not done, socket connected with client trigger POLLIN|POLLOUT continuously after peer(client) use `sdw` or `close` to send a FIN
-					//printf("EOF\n");
-					fflush(stdout);//absolutely necessary, no output otherwise
-				}
-				r--;
-			}
-			if (fds[2].revents & POLLOUT)
-				r--;
+			handle(fds, 2, &flag);
 		}
 	}
 }
 
-void my_poll2(int fd)
+void cpoll(int fd)
 {
 	int r;
+	static int flag = 0;
 	struct pollfd fds[2] = {
-		[0] = {
-			.fd = fd,
-			.events = POLLIN,// | POLLHUP is ignored in events so remove it!
-		},
-		[1] = {
+		[STDIN] = {
 			.fd = STDIN_FILENO,
 			.events = POLLIN,
 		},
+		[CONNECTED] = {
+			.fd = fd,
+			.events = POLLIN,// | POLLHUP is ignored in events so remove it!
+		},
 	};
 	while (1) {
-		char buf[4096];
-		int ret;
 		r = poll(fds, ARRAY_SIZE(fds), -1);
 		printf("%d: %x %x\n", r, fds[0].revents, fds[1].revents);
-		if (r > 0) {
-			if (fds[0].revents & POLLNVAL) {
-				warnx("***POLLNVAL fd %d, fixed to -1", fds[0].fd);
-				fds[0].fd = -1;
-			}
-			if (fds[0].fd != -1 && fds[0].revents & (POLLIN | POLLHUP)) {
-				ret = recv(fds[0].fd, buf, sizeof(buf), 0);
-				if (ret > 0)
-					write(STDIN_FILENO, buf, ret);
-				if (fds[0].revents & POLLHUP) {
-					ret = sprintf(buf, "server POLLHUP");
-					write(STDOUT_FILENO, buf, ret);
-					close(fds[0].fd);
-					fds[0].fd = -1;
-				}
-				r--;
-			}
-			if (fds[1].revents & POLLIN) {
-				int s = fds[0].fd;
-				ret = read(STDIN_FILENO, buf, sizeof(buf));
-				//printf("read %d\n", ret);
-				if (ret > 0 && s >= 0) {
-					int r;
-					if (!strncmp(buf, "sdr", 3)) {//strncmp's n should not be ret as ret contains '\n'
-//c=>s no any packet sent from c to s after the read's shutdown
-						if ((r = shutdown(s, SHUT_RD)) == -1)
-							warn("sdr error");
-						else
-							printf("sdr %d\n", r);
-					} else if (!strncmp(buf, "sdw", 3)) {
-/*c=>s [FIN,ACK];
-s=>c [ACK]; if server ^c then, POLLHUP is got*/
-						if ((r = shutdown(s, SHUT_WR)) == -1)
-							warn("sdw error");
-						else
-							printf("sdw %d\n", r);
-					} else if (!strncmp(buf, "close", 5)) {
-/*c=>s [FIN,ACK];
-s=>c [ACK]; if server ^c then, No ANY POLLHUP can be got by client*/
-						r = close(s);
-						printf("close %d\n", r);
-						fds[0].fd = -1;
-					} else if (!strncmp(buf, "CLOSE", 5)) {//no POLLHUP after its close()
-						r = close(s);
-						fds[0].revents = -1;//it will be written by POLLNVAL in next poll()
-						printf("CLOSE %d\n", r);
-					} else
-						send(s, buf, ret, 0);
-				}
-				r--;
-			}
-		}		
+		if (r > 0)
+			handle(fds, 2, &flag);
 	}
 }
 void connect_server(const char *host, const char *port, struct addrinfo hints)
@@ -523,7 +469,7 @@ void connect_server(const char *host, const char *port, struct addrinfo hints)
 			err(1, "socket");
 		if (connect(s, res0->ai_addr, res0->ai_addrlen) == -1)
 			err(1, "connect");
-		my_poll2(s);
+		cpoll(s);
 	}
 	freeaddrinfo(res);
 }
@@ -589,7 +535,7 @@ preparing for getaddrinfo() since nc can be called with a host name like:
 		s = local_listen(host, *uport, hints);
 		if (s < 0)
 			errx(1, "setup server error");
-		my_poll(s);		
+		spoll(s);
 	} else {
 		connect_server(host, *uport, hints);
 	}
