69fef4729b647ed54c34160003f8666f1d01eba7
rednoax <rednoax@gmail.com>
2021-04-28 03:33:59 -0700

    add read() and other test comment

--- taptun/taptun.c
+++ taptun/taptun.c
@@ -6,18 +6,19 @@
 #include <stdio.h>
 #include <err.h>
 #include <errno.h>
+#include <poll.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/ioctl.h>
 #include <linux/if.h>
 #include <linux/if_tun.h>
-
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
 int debug = 1;
 enum {
 	SERVER, CLIENT
 };
 
-int tun_alloc(char *if_name, int flags)
+int tun_alloc(char *if_name, int flags)//if_name will be overwritten with the real device name after ioctl returns ok
 {
 	struct ifreq ifr;
 	const char *dev = "/dev/net/tun";
@@ -28,12 +29,37 @@ int tun_alloc(char *if_name, int flags)
 	memset(&ifr, 0, sizeof(ifr));
 	ifr.ifr_flags = flags;
 	strncpy(ifr.ifr_name, if_name, IFNAMSIZ - 1);
-	if ((ret = ioctl(fd, TUNSETIFF, &ifr)) < 0) {
+	if ((ret = ioctl(fd, TUNSETIFF, &ifr)) < 0) {//ok:>=0 err:-1
 		close(fd);
-		err(1, "ioctl %s error %d", dev, errno);
-	}
+		err(1, "ioctl %s error %d", dev, errno);//if no sudo, it ends up here with 'Operation not permitted'.If `tunctl -u rednoah -t tap0`:TUNSETIFF: Operation not permitted
+	} else
+		printf("ioctl '%s'=>'%s': %d\n", if_name, ifr.ifr_name, ret);
 	strncpy(if_name, ifr.ifr_name, IFNAMSIZ - 1);
 	return fd;
+}
+
+char buf[1<<20];
+void watch(int fd)
+{
+	int i, r;
+	struct pollfd fds[] = {
+		[0] = {
+			.fd = fd,
+			.events = POLLIN,
+		},
+	};
+	int cfd = open("data.cap", O_CREAT | O_TRUNC, 0644);
+	while ((r = poll(fds, ARRAY_SIZE(fds), -1)) > 0) {
+		for (i = 0; i < ARRAY_SIZE(fds); i++) {
+			printf("%d: %x\n", fds[i].fd, fds[i].revents);
+		}
+		if (fds[0].revents & POLLIN) {
+			r = read(fds[0].fd, buf, sizeof(buf));
+			write(cfd, buf, r);
+			fsync(cfd);
+		}
+	}
+	err(1, "poll() error %d", r);//-1 or 0(timeout)
 }
 
 void do_debug(const char *fmt, ...)
@@ -68,7 +94,7 @@ int main(int argc, char *argv[])
 	char if_name[IFNAMSIZ] = {0};
 	const char* remote_ip = NULL;
 	int cliserv = -1;
-	int port = 1080, flags = IFF_TAP;
+	int port = 1080, flags = IFF_TAP;//2layer ethernet frame default, rather than 3layer IP packet
 	while ((c = getopt(argc, argv, "i:sc:p:uahd")) != -1) {
 		switch (c) {
 		case 'd':
@@ -107,20 +133,36 @@ int main(int argc, char *argv[])
 		my_err("Too many options!\n");
 		usage();
 	}
+#if 0
 	if (*if_name == '\0') {
 		my_err("Must specify interface name!\n");
 		usage();
-	} else if (cliserv < 0) {
+	}
+	else if (cliserv < 0) {
 		my_err("Must specify client or server mode!\n");
 		usage();
 	} else if (cliserv == CLIENT && !remote_ip) {
 		my_err("Must specify server address!\n");
 		usage();
 	}
+#endif
 	if ((tap_fd = tun_alloc(if_name, flags | IFF_NO_PI)) < 0) {
 		my_err("Error connecting to %s interface %s\n", flags & IFF_TUN? "TUN": "TAP", if_name);
 		exit(1);
 	}
 	do_debug("connected to %s\n", if_name);
+/*
+test when commentA enabled:
+0.If no any tapX in `ifconfig -a`, `sudo ./a.out` w/t '-i' will generate 'tap0' only during this
+program's running. Once the program exit(), 'tap0' disappeared.(When the program closes the file
+descriptor, the network device and all corresponding routes will disappear.)
+1. If the 'tap0' has existed(`sudo tunctl -u rednoah -t tap0`, no need to ifconfig tap0 ip) before
+`sudo ./a.out -i tap0`, tun_alloc() will not create new tap device and ioctl of 'tap0' return 0(ok).
+In other word the program will not create new tapX device but use the specified existed tap0 directly.
+2. If the tap0 has not been setup like 1, then `sudo ./a.out ` will create tap1, which only exists
+during this program's running.
+*/
+	//sleep(10); exit(1);//commentA
+	watch(tap_fd);
 	return 0;
 }
