7166eb2ec85c0ae4517c5a58295963c87df31675
rednoah <rednoax@gmail.com>
2021-06-12 16:16:59 +0800

    tuned

--- make/TGMB/u10.mk
+++ make/TGMB/u10.mk
@@ -10,4 +10,9 @@ $(info $(call dump,	1 ,2 , 3))#the 1st arg has a preceding \t and a trailing spa
  the 1st argument.
 $(info $(call dump, 1	, 2,3 ))#the 1st arg has a preceding space and a trailing \t,all of them is preserved as the value of \
  the 1st argument.
+ 
+$(info (${findstring 	1,1 2}))#there is a space and a \t before the 1st arg of findstring, they're all ignored so the real 1st\
+ argument is just one character '1'
+$(info ($(findstring 1 ,2 1)))#print () as the trailing space of 1st arg is preserved as value of $1
+$(info ($(findstring 1	,1	2)))#print (1\t),the 1st arg has a trailing \t,the 2nd arg is "1\t2" so findstring recognize it is matched
 all:
