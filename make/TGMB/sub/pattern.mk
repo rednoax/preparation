02e58c38598e57df9984078e71e559af72dd27ec
rednoah <rednoax@gmail.com>
2021-06-24 16:34:03 +0800

    % must be >=1 chars

---
+++ make/TGMB/sub/pattern.mk
@@ -0,0 +1,20 @@
+.PHONY:all
+all:.out;
+
+#comment A:if disable the following ifeq block\
+$ make -f pattern.mk\
+make: *** No rule to make target '.out', needed by 'all'.  Stop.
+#so % can't match zero char, % means >=1 chars is a must, if `mv .txt a.txt` then all:a.txt, this makefile works for `make all`
+%.out:%.txt
+	cp $< $@
+
+ifeq (1,1)#if disable the following, see comment A
+#$ make -f pattern.mk\
+make: Circular pattern.mk.out <- pattern.mk dependency dropped.\
+make: Circular .txt.out <- .txt dependency dropped.\
+cp .txt .out
+.out:.txt
+	cp $< $@
+endif
+clean:
+	rm -rf .out *.out
