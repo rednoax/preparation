211dd6463a3ac625ac60b5a31e2022b883d092c7
rednoah <rednoax@gmail.com>
2021-06-26 11:12:06 +0800

    2 types of empty recipe to force remake

--- make/TGMB/sub/implicit.mk
+++ make/TGMB/sub/implicit.mk
@@ -1,4 +1,15 @@
 $(info $(LINK.c),$^,$(LOADLIBES),$(LDLIBS))
 $(info $(COMPILE.c),$(OUTPUT_OPTION))
-.PHONY:
-all:;
+#Each time running `make all`, : will AL be run.\
+REASON:a.out's rule has no prerequisite, and the target of the rule, ie a.out,is a nonexisting\
+file. So its reipes,even empty, is run, then make imagines the target a.out to have been updated.\
+This implies that all targets depending on a.out will AL have their recipe run.
+all:a.out
+	:
+a.out:;
+
+#Each time running `make all2`,: will AL be run.See above for the reason.\
+Here Force's rule's recipe is empty, which is equivalent to FORCE:;
+all2:FORCE
+	:
+FORCE:
