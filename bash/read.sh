d202a71732ccb91c70725bfb9ac594cfd1affd93
rednoah <rednoax@gmail.com>
2021-07-08 13:23:35 +0800

    add comments

--- bash/read.sh
+++ bash/read.sh
@@ -1,3 +1,7 @@
+#the following can't be [3]<input.txt:
+#./read.sh: line 1: [3]: command not found
+#./read.sh: line 3: read: 3: invalid file descriptor: Bad file descriptor
+
 exec 3<input.txt
 count=0
 while read -u 3 var
