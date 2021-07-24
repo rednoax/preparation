7969fc7cb8959539601b20ddc3eb4f9322501c99
rednoah <rednoax@gmail.com>
2021-07-24 13:32:19 +0800

    Add comments

--- eos2020/ch2/c2.1/ts.s
+++ eos2020/ch2/c2.1/ts.s
@@ -6,7 +6,7 @@ start:
 	mov r0, #1
 	mov r1, #2
 	add r1, r1, r0
-	ldr r2, =result @both ts.o and ts.elf: ldr r2, [pc, #4], see comment 1
+	ldr r2, =result @both ts.o and ts.elf: ldr r2, [pc, #4], [pc,#4] holds LABEL result's address,see comment 1
 	str r1, [r2]
 stop:
 	@adr r3, result @r3 = pc + offset, BUT: ts.s:12: Error: symbol .data is in a different section
@@ -23,6 +23,29 @@ dummy:
 	.word 0
 //
 /*comment 1
+$ arm-none-eabi-objdump -d t.elf
+
+t.elf:     file format elf32-littlearm
+
+Disassembly of section .text:
+
+00010000 <start>:
+   10000:       e3a00001        mov     r0, #1
+   10004:       e3a01002        mov     r1, #2
+   10008:       e0811000        add     r1, r1, r0
+   1000c:       e59f2004        ldr     r2, [pc, #4]    ; 10018 <stop+0x4>
+   10010:       e5821000        str     r1, [r2]
+
+00010014 <stop>:
+   10014:       eafffffe        b       10014 <stop>
+   10018:       0001001c        .word   0x0001001c<--store synbol 'result' value: ie its address
+
+$ arm-none-eabi-nm t.elf
+00010020 n dummy
+0001001c d result
+00010000 T start
+00010014 t stop
+
 arm has cannot directly modify the conent in mem, while x86 can do it in one single instrcution.
 When arm needs to modify content of memory, it may need to get the address of the mem in ways like:
 1. ldr reg, =label
