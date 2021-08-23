3a99c1165e1b3a191fda3ac241a676857aeb035b
rednoah <rednoax@gmail.com>
2021-08-23 15:49:42 +0800

    add a new file framework

--- modules/ptdump_debugfs.c
+++ modules/ptdump_debugfs.c
@@ -14,11 +14,20 @@ static int ptdump_show(struct seq_file *m, void *v)
 }
 DEFINE_SHOW_ATTRIBUTE(ptdump);
 
+static int cptdump_show(struct seq_file *m, void *v)
+{
+	struct ptdump_info *info = m->private;
+	cptdump_walk_pgd(m, info);
+	return 0;
+}
+DEFINE_SHOW_ATTRIBUTE(cptdump);
+
 static struct dentry *ptdump_debug_root;
 void __init ptdump_debugfs_register(struct ptdump_info *info, const char *name)
 {
 	ptdump_debug_root = debugfs_create_dir("ptdump", NULL);
 	debugfs_create_file(name, 0444, ptdump_debug_root, info, &ptdump_fops);
+	debugfs_create_file("cPT", 0444, ptdump_debug_root, info, &cptdump_fops);
 }
 
 void ptdump_debugfs_unregister()
