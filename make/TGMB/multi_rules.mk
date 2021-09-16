92f45f4cc1df56b8f0b6ff71514bdcb9d41a517d
rednoah <rednoax@gmail.com>
2021-09-16 17:58:36 +0800

    % in target must be >=1 chars

--- make/TGMB/multi_rules.mk
+++ make/TGMB/multi_rules.mk
@@ -3,6 +3,17 @@
 $ make -f multi_rules.mk mini2440_config\
 : 2
 #more specific rule wins
+
+#$make -f multi_rules.mk\
+: 2
+
+#$make -f multi_rules.mk _config \
+make: *** No rule to make target '_config'.  Stop.\
+\
+shell return 2
+#So the % in target can ONLY match >=1 chars
+#$make -f multi_rules.mk 1_config\
+: 1
 %_config:
 	: 1
 mini2440_config:
