264580178993b84f9c7c068bf3e45e9b01dfdb2a
rednoah <rednoax@gmail.com>
2021-09-17 19:28:54 +0800

    comments

--- make/TGMB/opt.mk
+++ make/TGMB/opt.mk
@@ -3,11 +3,15 @@ vars=MAKEFLAGS
 envbeforemakecons:=[$(MAKELEVEL):$(shell printenv|grep $(vars))]
 aftermakecons:=[$(MAKELEVEL):$(MAKEFLAGS)]
 #MAKEFLAGS's road: \
-1. for LN make, N>=1, the parent make transferred MAKEFLAGS in program's env list(C 's main(,,char **envp));\
- then LN make cons will add option from LN cmd line option, if any.\
-2. when LN make prepares MAKEFLAGS for L(N+1) child make, it uses the value of 1, plus its own explicitly value\
- from MAKEFLAGS+=..., if any.\
-3. for L0 make, it's the same as 1+2, see "make test2" example.
+1. for LN make(N>=1), its parent L(N-1) make AL transfers MAKEFLAGS in LN make program's env list(via C's main(,,char **envp));\
+ then LN make cons will add option from LN make's cmd line option, if any.\
+2. when LN(N>=1) make prepares MAKEFLAGS for L(N+1) child make, it uses the value of 1, plus its own explicitly value\
+ from MAKEFLAGS+=..., if any. The transferring is AL via environment.\
+3. for L0 make, it's the same as 1+2, Note in 1 MAKEFLAGS can still be from environment even L0 make has no parent make.\
+eg `export MAKEFLAGS=xx;make` or `MAKEFLAGS=xx make`.\
+IN BRIEF: child make's env list AL contains MAKEFLAGS transferred from parent make. For any level's make (LN N>=0) the MAKEFLAGS is\
+(environment transferred+some command line options,if any+file variable like MAKEFLAGS+=-rR,if any)\
+see "make test2" example.
 show=$(info $(envbeforemakecons)=>$(aftermakecons)=>[$(MAKELEVEL):$(MAKEFLAGS)])#initial env for make=>phase 1=>phase2
 CMD=$(MAKE) -f opt.mk
 
@@ -26,10 +30,18 @@ endif
 ifeq ($(MAKECMDGOALS),test2)
 MAKEFLAGS+=-r
 test2:
-	:$(show)
+	@printenv|grep MAKEFLAGS$(show)
+ifeq ($(MAKELEVEL) , 0)
+	$(MAKE) -f opt.mk test2
+endif	
 #$ MAKEFLAGS=k make -f opt.mk -i test2 \
 [0:MAKEFLAGS=k]=>[0:ik]=>[0:ikr]<-initial env has k,make cons adds cmd line option -i,phase 2 add -r from file +=\
-:
+MAKEFLAGS=ikr <--child process env list contains the MAKEFLAGS transferred by L0 make\
+make -f opt.mk test2 <--this child process is a real make whose env variable MAKECMDGOALS value is ikr\
+make[1]: Entering directory '/home/rednoah/opt/preparation/make/TGMB' \
+[1:MAKEFLAGS=ikr]=>[1:ikrw]=>[1:ikrw]<-‘-w’ is turned on automatically when you use the ‘-C’ option, and in sub-makes. \
+MAKEFLAGS=ikrw <-L2 process, even not make, has been transferred with MAKEFLAGS in its env list\
+make[1]: Leaving directory '/home/rednoah/opt/preparation/make/TGMB'
 endif
 
 #-s will stop:1.recipe command's display; 2.-w(which enable Entering/Leaving pairs display)\
