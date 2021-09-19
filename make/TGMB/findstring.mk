c53b19091f7074e87fbb0975463eebd076013cf5
rednoah <rednoax@gmail.com>
2021-09-19 09:37:14 +0800

    add MAKECMDGOALS tset

--- make/TGMB/findstring.mk
+++ make/TGMB/findstring.mk
@@ -1,3 +1,10 @@
 $(info [$(findstring a,aa a)])#a
 $(info [$(findstring a,aa)])#a
-all:
+$(info [$(findstring s,stella)])#s
+$(MAKECMDGOALS):
+#make -f findstring.mk
+#make: *** No targets.  Stop.\
+shell returned 2
+	@echo $@
+	printenv | grep MAKECMDGOALS
+#there will be no MAKECMDGOALS in child process's env
