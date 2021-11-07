084b15e61a39462cd4d6cb1d9b19aea8dcc1bc1b
rednoah <rednoax@gmail.com>
2021-11-07 17:08:08 +0800

    add comment

--- assembly/x86/qemu.sh
+++ assembly/x86/qemu.sh
@@ -3,6 +3,7 @@
 #NOTE:
 #1. simplified 'set arch i8086' is equivalent to 'set architecture i8086'
 #2.'set architecture i8086' should be AFTER 'target remote:1234', there is warning otherwise.
+#3. display/i $pc is not right 8086 assembly and eip w/t cs is used as break point address
 qemu-system-i386 -m 16M  -boot c -hda $*
 #the above can be simplified to: qemu-system-i386 -hda $*
 
