f607dc40c9010795b51088f11e2a7a0a3473c6e9
rednoah <rednoax@gmail.com>
2021-11-07 11:57:27 +0800

    .text fixed to be 7c00 for gdb;fix qemu.sh

--- assembly/x86/qemu.sh
+++ assembly/x86/qemu.sh
@@ -1,1 +1,11 @@
-qemu-system-i386 -m 16M -boot c -enable-kvm -hda $*
+#usage: ./qemu.sh 8086.vhd -S -s
+#gdb -ex 'target remote:1234' -ex 'set architecture i8086' -ex 'break *0x7c00' -ex 'layout regs' -ex 'layout src'
+#NOTE:
+#1. simplified 'set arch i8086' is equivalent to 'set architecture i8086'
+#2.'set architecture i8086' should be AFTER 'target remote:1234', there is warning otherwise.
+qemu-system-i386 -m 16M  -boot c -hda $*
+#the above can be simplified to: qemu-system-i386 -hda $*
+
+#-enable-kvm option makes 'b *0x7c00' not take effect, the breakpoint is never hit.
+#qemu-system-i386 -m 16M -boot c -enable-kvm -hda $*
+
