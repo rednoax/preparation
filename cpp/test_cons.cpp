2f11b4ebcd5698d9f3a72355eb36204a151ea54a
rednoah <rednoax@gmail.com>
2021-11-15 10:36:53 +0800

    test default cons and cin

--- cpp/test_cons.cpp
+++ cpp/test_cons.cpp
@@ -1,10 +1,28 @@
 #include <iostream>
 
+class A
+{
+public:
+	int a;
+	int *p;
+	/*
+	A(int i)
+	{
+		a = i;
+	}
+	*/
+	void print()
+	{
+		printf("%x %p\n", a, p);
+	}
+};
+A ma;
 int main()
 {
 	int i;
 	char c;
-	std::cin >> i >> c;
-	printf("%d %c\n", i, c);
+	ma.print();//0 0
+	std::cin >> i >> c;//'\t\t 304\t\t 20'=>304 50
+	printf("%d %d\n", i, c);
 	return 0;
 }
