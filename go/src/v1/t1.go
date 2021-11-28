e2676ee838d8e8899146bff796f297a02d16a621
rednoah <rednoax@gmail.com>
2021-11-28 12:18:24 +0800

    go build fails

---
+++ go/src/v1/t1.go
@@ -0,0 +1,8 @@
+package main
+import (
+	"fmt"
+)
+func main() {
+	var a = 1
+	fmt.Print("a=",a)
+}
---
+++ go/src/v1/t2.go
@@ -0,0 +1,8 @@
+package main2
+import (
+	"fmt"
+)
+func mxx() {
+	var a int = 3498
+	fmt.Println("a = ", a)
+}
