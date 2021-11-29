738c99684efde48f77ecb2c849c8816ad64924cd
rednoah <rednoax@gmail.com>
2021-11-29 11:16:43 +0800

    use a general 8086.vhd for virtualbox/qemu/bochs

--- assembly/x86/qemu.sh
+++ assembly/x86/qemu.sh
@@ -5,7 +5,10 @@
 #2.'set architecture i8086' should be AFTER 'target remote:1234', there is warning otherwise.
 #3. display/i $pc is not right 8086 assembly and eip w/t cs is used as break point address
 qemu-system-i386 -m 16M  -boot c -hda $* -monitor stdio
-#the above can be simplified to: qemu-system-i386 -hda $*
+#the above can be simplified to:
+# qemu-system-i386 -hda $*
+#or more simple
+# qemu-system-i386 8086.vhd
 
 #-enable-kvm option makes 'b *0x7c00' not take effect, the breakpoint is never hit.
 #qemu-system-i386 -m 16M -boot c -enable-kvm -hda $*
