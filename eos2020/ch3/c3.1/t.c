78d395fc1fe875062076c6fec23fdc3868b683ca
rednoah <rednoax@gmail.com>
2021-12-21 21:42:35 +0800

    debug for /

--- eos2020/ch3/c3.1/t.c
+++ eos2020/ch3/c3.1/t.c
@@ -51,7 +51,64 @@ is exactly == symbol value in symbol table.
 	*k = 0x80868086;
     kprintf("%x==%x==%x\n", i, *j, *k);
 }
+#if 0
+/*
+1.if '-mabi=apcs-gnu' is used, the divide function name is changed from __aeabi_idiv to __divsi3.
+If the following empty __divsi3 is added, the `gcc -c t.c` will be ok and their objdump is shown below:
+00001288 <__divsi3>:
+    1288:       e52db004        push    {fp}            ; (str fp, [sp, #-4]!)
+    128c:       e28db000        add     fp, sp, #0
+    1290:       e24dd004        sub     sp, sp, #4
+    1294:       e50b0004        str     r0, [fp, #-4]
+    1298:       e3a03000        mov     r3, #0
+    129c:       e1a00003        mov     r0, r3
+    12a0:       e28bd000        add     sp, fp, #0
+    12a4:       e49db004        pop     {fp}            ; (ldr fp, [sp], #4)
+    12a8:       e12fff1e        bx      lr
 
+000012ac <divide>:
+    12ac:       e92d4810        push    {r4, fp, lr}
+    12b0:       e28db008        add     fp, sp, #8
+    12b4:       e24dd004        sub     sp, sp, #4
+    12b8:       e50b000c        str     r0, [fp, #-12]
+    12bc:       e3a03000        mov     r3, #0
+    12c0:       e5933000        ldr     r3, [r3]
+    12c4:       e3a04a01        mov     r4, #4096       ; 0x1000
+    12c8:       e1a01003        mov     r1, r3
+    12cc:       e51b000c        ldr     r0, [fp, #-12]
+    12d0:       ebfffffe        bl      1288 <__divsi3>
+    12d4:       e1a03000        mov     r3, r0
+    12d8:       e5843000        str     r3, [r4]
+    12dc:       e1a00000        nop                     ; (mov r0, r0)
+    12e0:       e24bd008        sub     sp, fp, #8
+    12e4:       e8bd8810        pop     {r4, fp, pc}
+2.if ts.S and t.c is compiled with different -mabi option, the link will fail:
+++ arm-none-eabi-gcc -c -g -Wall -mcpu=arm926ej-s ts.S -o ts.o <=-mabi=aapcs-linux is used by default
+++ arm-none-eabi-gcc -c -g -Wall -mcpu=arm926ej-s -mabi=apcs-gnu t.c -fno-builtin -o t.o <= -mabi=apcs-gnu is explicitly used
+++ func arm-none-eabi-ld -T t.ld ts.o t.o -Map t.map -o t.elf
+++ arm-none-eabi-ld -T t.ld ts.o t.o -Map t.map -o t.elf
+arm-none-eabi-ld: error: source object t.o has EABI version 0, but target t.elf has EABI version 5<=different abi cause link error
+
+*/
+int __divsi3(int i)
+{
+	return 0;
+}
+/*
+if no '-mabi=apcs-gnu' during `gcc -c`, '-mabi=aapcs-linux' is used by default.
+The following is error when no '-mabi' option is used:
+1. if no '-lgcc -L $(dirname `arm-none-eabi-gcc -print-libgcc-file-name`)' in ld, there is error:
+t.c:60: undefined reference to `__aeabi_idiv'
+2. change / to %:
+/home/rednoah/opt/preparation/eos2020/ch3/c3.1/t.c:61: undefined reference to `__aeabi_idivmod'
+3. if changing to unsigned divide:
+undefined reference to `__aeabi_uidiv'
+*/
+void divide(int i)
+{
+	*(int*)0x1000 = i / *(int*)0;
+}
+#endif
 int main()
 {
 	int i;
