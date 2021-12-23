9bb90a1a46871987143bd71eaf315716e32cb520
rednoah <rednoax@gmail.com>
2021-12-23 17:29:55 +0800

    'MHZ' arg problem

---
+++ assembly/arm/string.c
@@ -0,0 +1,12 @@
+void printascii(char *);
+
+void test0()
+{
+    printascii('360');//'360':(0x33<<16)+(0x36<<8)+0x30=3356208, r0==3356208
+    printascii('MHZ');//('M'<<16)+('H'<<8)+('Z')=5064794, r0=5064794, ie 0x4d485a in range [0,+128M) that is undocumented
+}
+
+void test1()
+{
+    printascii("MHZ");//r0:an address holding .asciz "MHZ"
+}
