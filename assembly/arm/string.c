a3da8d80718c753416246881147f624d084a70d6
rednoah <rednoax@gmail.com>
2021-12-23 17:32:51 +0800

    comments

--- assembly/arm/string.c
+++ assembly/arm/string.c
@@ -3,6 +3,7 @@ void printascii(char *);
 void test0()
 {
     printascii('360');//'360':(0x33<<16)+(0x36<<8)+0x30=3356208, r0==3356208
+//the following can't print string "MHZ"!
     printascii('MHZ');//('M'<<16)+('H'<<8)+('Z')=5064794, r0=5064794, ie 0x4d485a in range [0,+128M) that is undocumented
 }
 
